4bf6d2c0e8abaa1594ce2b31a955f88957c1fe8d
snail-Py <cs.legoff@gmail.com>
2022-02-18 12:34:30 +0800

    Date of today automatically selected when creating a new post

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -1,13 +1,14 @@
 from django import forms
 from django.forms import ModelForm
 from .models import NewsStory
-from datetime import datetime
+from django.utils import timezone, dateformat
+
 
 class StoryForm(ModelForm):
      class Meta:
          model = NewsStory
          fields = ['title', 'pub_date', 'content', 'tag', 'image']
-         widgets = {'pub_date': forms.DateInput(format=('%m/%d/%Y'),
+         widgets = {'pub_date': forms.DateInput(format=('%Y-%m-%d'),
 attrs={'class':'form-control', 'placeholder':'Select a date',
-'type':'date'}),
+'type':'date', "value": dateformat.format(timezone.now(), 'Y-m-d')}),
 }
